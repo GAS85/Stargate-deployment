04899d71b9e114e5f5c24cd79efe93375897fc47
Zdravko Komitov <zdravko.komitov@vereign.com>
2026-04-29 11:44:15 +0300

    Add dashboard,customer service,apisix,keycloak, nginx reverse proxy

--- docker-compose/init/postgres/01-create-databases.sql
+++ docker-compose/init/postgres/01-create-databases.sql
@@ -3,9 +3,15 @@ CREATE DATABASE smimekeys_client;
 CREATE DATABASE policy;
 CREATE DATABASE idagent;
 CREATE DATABASE mxengine;
+CREATE DATABASE keycloak;
+CREATE DATABASE customers;
+CREATE DATABASE dashboard;
 
 -- Grant privileges (using default postgres user)
 GRANT ALL PRIVILEGES ON DATABASE smimekeys_client TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE policy TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE idagent TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE mxengine TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE customers TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE dashboard TO postgres;
