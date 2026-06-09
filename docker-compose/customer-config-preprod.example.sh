772e5c6390a0b2c3f04d43803d3e212825add2df
Petar Dobrinov <petar.dobrinov@vereign.com>
2026-06-09 16:09:31 +0300

    Disable developer pages in dashboard configuration for production and pre-production templates

--- docker-compose/customer-config-preprod.example.sh
+++ docker-compose/customer-config-preprod.example.sh
@@ -142,7 +142,7 @@ KEYCLOAK_PUBLIC_URL=""      # Default: https://<SERVER_STATIC_IP>:8180
 DASHBOARD_PUBLIC_URL=""     # Default: https://<SERVER_STATIC_IP>
 
 # Show developer pages in the dashboard UI
-DASHBOARD_SHOW_DEV_PAGES="true"
+DASHBOARD_SHOW_DEV_PAGES="false"
 
 # Root instance URL (the central APISIX gateway that this instance connects to)
 DASHBOARD_ROOT_URL="https://apisix.hindev.ch"
